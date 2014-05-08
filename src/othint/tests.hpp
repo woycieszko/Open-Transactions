50c808ecb445939c83cf97b69c138ef037339c25
rfree2 <rfree@op.pl>
2014-05-08 14:48:21 +0200

    wip tests

---
+++ src/othint/tests.hpp
@@ -0,0 +1,26 @@
+/* See other files here for the LICENCE that applies here. */
+/*
+Template for new files, replace word "template" and later delete this line here.
+*/
+
+#ifndef INCLUDE_OT_NEWCLI_tests
+#define INCLUDE_OT_NEWCLI_tests
+
+#include "lib_common1.hpp"
+
+namespace nOT {
+namespace nTests {
+
+INJECT_OT_COMMON_USING_NAMESPACE_COMMON_2; // <=== namespaces
+
+bool testcase_run_all_tests();
+
+void exampleOfOT();
+
+int main_main(int argc, char **argv); // some tests will execute the main... e.g. against errors in args parsing TODO move to namespace
+
+} // namespace nTests
+} // namespace nOT
+
+#endif
+
