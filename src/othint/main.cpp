f88aa8fd68c47a85b87cfb180c56f79dd7e736ec
rfree2 <rfree@op.pl>
2014-05-08 16:58:00 +0200

    small changes

--- src/othint/main.cpp
+++ src/othint/main.cpp
@@ -7,26 +7,23 @@
 INJECT_OT_COMMON_USING_NAMESPACE_COMMON_1
 
 int main(int argc, const char **argv) {
+	int ret=0;
 
-	nOT::nNewcli::cOTCli application;
-	int ret = application.Run(argc,argv);
+	try {
+		ret=1; // if aborted then this indicated error
+		nOT::nNewcli::cOTCli application;
+		int ret = application.Run(argc,argv);
+	}
+	catch (const std::exception &e) {
+  	_erro("\n*** Captured exception:" << e.what());
+	}
+	catch (...) {
+  	_erro("\n*** Captured UNKNOWN exception:");
+	}
 
 	// nOT::nTests::exampleOfOT(); // TODO from script
 	// nOT::nTests::testcase_run_all_tests(); // TODO from script
 
 	return ret;
 }
-
-/*
-
-	catch(const std::exception &e) {
-		_erro("\n*** The testcases code thrown an exception: " << e.what());
-	}
-	catch(...) {
-		_erro("\n*** The testcases code thrown an UNKNOWN exception!");
-	}
-
-*/
-
-// #####################################################################
 
